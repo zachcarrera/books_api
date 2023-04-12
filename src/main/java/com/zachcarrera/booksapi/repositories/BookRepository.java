f8b5b250e0d4c9c3aa218c65122bb01a62d91517
Zach Carrera <zachcarrera@gmail.com>
2023-04-11 22:39:35 -0700

    add BookRepository interface

---
+++ src/main/java/com/zachcarrera/booksapi/repositories/BookRepository.java
@@ -0,0 +1,15 @@
+package com.zachcarrera.booksapi.repositories;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.zachcarrera.booksapi.models.Book;
+
+@Repository
+public interface BookRepository extends CrudRepository<Book, Long>{
+	
+	List<Book> findAll();
+	
+}
