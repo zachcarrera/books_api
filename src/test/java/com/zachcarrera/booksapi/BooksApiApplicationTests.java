cec7c9ff00ed1d1397dc48a9f4113b11941e9da5
Zach Carrera <zachcarrera@gmail.com>
2023-04-11 22:40:23 -0700

    add test directory

---
+++ src/test/java/com/zachcarrera/booksapi/BooksApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zachcarrera.booksapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
